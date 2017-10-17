743a20aec6a046d092d77ea45315f2bc85aaab4f
Kim Leung <kleung@contently.com>
2017-10-17 09:08:23 -0400

    container running w/ exposed port 4444, spec demo.js is green

--- tests/demo.js
+++ tests/demo.js
@@ -1,13 +1,13 @@
 module.exports = {
-  'Demo test Google' : function (browser) {
+  "Demo test Google" : function (browser) {
     browser
-      .url('http://www.google.com')
-      .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
+      .url("http://www.google.com")
+      .waitForElementVisible("body", 1000)
+      .setValue("input[type=text]", "nightwatch")
+      .waitForElementVisible("input[value='Google Search']", 1000)
+      .click("input[value='Google Search']")
       .pause(1000)
-      .assert.containsText('#main', 'Night Watch')
+      .assert.containsText("#main", "Night Watch")
       .end();
   }
 }
