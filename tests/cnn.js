c12cf51c3572dd47cbac9e4d1bc2d715145955d6
Lo Leung <kalefive.me@gmail.com>
2018-02-07 00:29:10 -0500

    add commands to readme

--- tests/cnn.js
+++ tests/cnn.js
@@ -3,7 +3,7 @@ module.exports = {
     browser
       .url("http://www.cnn.com")
       .waitForElementVisible("body", 1000)
-      // .saveScreenshot("./Screenshots/new/cnn_page.png")
+      .saveScreenshot("./Screenshots/new/cnn_page.png")
       // .pause(1000)
       .end();
   }
